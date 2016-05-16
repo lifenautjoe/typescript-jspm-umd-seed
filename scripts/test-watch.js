bb24725175b49933d94373b10dfa8beaca3646ea
Joel Hernandez <involvmnt@gmail.com>
2016-05-16 20:26:02 +0200

    style(test-watch.js): conform to tslint

--- scripts/test-watch.js
+++ scripts/test-watch.js
@@ -17,12 +17,12 @@ var karmaRunnerRun = require('./karma-runner-run/karma-runner-run');
 
 cli.parse({
     src: ['A glob pattern to match source files which trigger a run on changed', 'string'],
-    srcExclude: ['A glob pattern to exclude source files','string'],
+    srcExclude: ['A glob pattern to exclude source files', 'string'],
     specs: ['A glob pattern to match specification files which trigger a run on changed', 'string'],
-    specsExclude: ['A glob pattern to exclude specification files','string'],
+    specsExclude: ['A glob pattern to exclude specification files', 'string'],
     bundleDest: ['The destination of the generated bundle', 'string'],
-    karmaConfigFile : ['The karma configuration file within the project directory','string','karma.config.js'],
-    jspmConfigFile : ['The jspm configuration file within the project directory','jspm.config.js'],
+    karmaConfigFile: ['The karma configuration file within the project directory', 'string', 'karma.config.js'],
+    jspmConfigFile: ['The jspm configuration file within the project directory', 'jspm.config.js'],
     hostname: ['h', 'The karma server hostname', 'string', 'localhost'],
     urlRoot: ['u', 'The karma server url root', 'string', '/'],
     port: ['p', 'The karma server port', 'int', 9876],
@@ -44,32 +44,32 @@ cli.main(function (args, options) {
     var port = options.port;
     var failOnEmptyTestSuite = options.failOnEmptyTestSuite;
     var karmaRunnerRunConfig = {
-        hostname : hostname,
-        urlRoot : urlRoot,
-        port : port,
-        failOnEmptyTestSuite : failOnEmptyTestSuite
+        hostname: hostname,
+        urlRoot: urlRoot,
+        port: port,
+        failOnEmptyTestSuite: failOnEmptyTestSuite
     };
 
-    function onError(err){
+    function onError(err) {
         cli.fatal(err);
     }
 
-    function makeServer(configFile,onReady){
-        return new Promise(function(resolve, reject){
+    function makeServer(configFile, onReady) {
+        return new Promise(function (resolve, reject) {
             var resolved;
             var server = new KarmaServer({
-                configFile : configFile,
-                hostname : hostname,
-                urlRoot : urlRoot,
-                port : port,
-                failOnEmptyTestSuite : failOnEmptyTestSuite
-            }, function(exitCode) {
+                configFile: configFile,
+                hostname: hostname,
+                urlRoot: urlRoot,
+                port: port,
+                failOnEmptyTestSuite: failOnEmptyTestSuite
+            }, function (exitCode) {
                 reject('Karma has exited with ' + exitCode);
             });
 
-            server.on('browsers_ready',function () {
+            server.on('browsers_ready', function () {
                 // There is no server_ready event..
-                if(!resolved) {
+                if (!resolved) {
                     resolve(server);
                     resolved = true;
                 }
@@ -80,53 +80,53 @@ cli.main(function (args, options) {
         });
     }
 
-    function runSpecs(){
-        return karmaRunnerRun(karmaRunnerRunConfig,cli);
+    function runSpecs() {
+        return karmaRunnerRun(karmaRunnerRunConfig, cli);
     }
 
-    function bundlePackage(){
+    function bundlePackage() {
         var jspmBuilder = new JspmBuilder();
         return jspmBuilder.loadConfig(jspmConfigFile).then(function () {
             return jspmBuilder.bundle('typescript-jspm-umd-seed', bundleDest);
         });
     }
 
-    function setSourceWatcher(){
-        chokidar.watch(src,{
-            ignored : srcExclude
-        }).on('change',function(file){
-            cli.info(['Source file',file,'has changed, re-bundling package'].join(' '));
+    function setSourceWatcher() {
+        chokidar.watch(src, {
+            ignored: srcExclude
+        }).on('change', function (file) {
+            cli.info(['Source file', file, 'has changed, re-bundling package'].join(' '));
             bundlePackage().catch(onError);
         });
     }
 
-    function setBundleWatcher(server){
-        chokidar.watch(bundleDest).on('change',function () {
+    function setBundleWatcher(server) {
+        chokidar.watch(bundleDest).on('change', function () {
             cli.info('Bundle changed, re-running specs');
-            server.refreshFiles().then(function(){
+            server.refreshFiles().then(function () {
                 return runSpecs();
             }).catch(onError);
         });
     }
 
-    function setSpecsWatcher(){
-        chokidar.watch(specs,{
-            ignored : specsExclude
-        }).on('change',function (file) {
-            cli.info(['Spec file',file,'has changed, re-running specs'].join(' '));
+    function setSpecsWatcher() {
+        chokidar.watch(specs, {
+            ignored: specsExclude
+        }).on('change', function (file) {
+            cli.info(['Spec file', file, 'has changed, re-running specs'].join(' '));
             runSpecs().catch(onError);
         });
     }
 
-    function setWatchers(server){
+    function setWatchers(server) {
         setBundleWatcher(server);
         setSpecsWatcher();
         setSourceWatcher();
     }
 
-    bundlePackage().then(function() {
-        return makeServer(karmaConfigFile,runSpecs);
-    }).then(function(server){
+    bundlePackage().then(function () {
+        return makeServer(karmaConfigFile, runSpecs);
+    }).then(function (server) {
         setWatchers(server);
     }).catch(onError);
 });
