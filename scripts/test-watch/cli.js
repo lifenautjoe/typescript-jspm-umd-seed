f8e03c6ebed178f81ca4a2b16daf438c411c4037
Joel Hernandez <involvmnt@gmail.com>
2016-07-16 19:43:13 +0200

    refactor(text-watch): remove logs

--- scripts/test-watch/cli.js
+++ scripts/test-watch/cli.js
@@ -29,9 +29,7 @@ cli.parse({
 });
 
 cli.main(function (args, options) {
-    console.log(options);
     testWatch(options).catch(function (err) {
-        console.log(err.message);
         cli.fatal(err);
     });
 });
